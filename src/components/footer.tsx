702c0aa6269040e648eb3916b00e2bf8c7177184
Jean-François Corriveau <jcorriveau23@gmail.com>
2024-05-14 12:44:58 -0400

    Fixed text color of footer

--- src/components/footer.tsx
+++ src/components/footer.tsx
@@ -15,35 +15,39 @@ export default async function Footer() {
     <footer className="mx-0 py-4 w-full">
       <div className="container grid grid-cols-2  gap-8">
         <div className="grid gap-1">
-          <h3 className="font-semibold">{t("FollowUs")}</h3>
+          <h3 className="font-bold">{t("FollowUs")}</h3>
           <Link
             href="https://twitter.com/hockeypool3"
-            className="flex items-center gap-2 hover:underline"
+            className="flex items-center gap-2 hover:underline text-muted-foreground"
           >
             <TwitterLogoIcon className="h-5 w-5" />
             Twitter
           </Link>
           <Link
             href="https://github.com/jcorriveau23/new-frontend-pool-nhl"
-            className="flex items-center gap-2 hover:underline"
+            className="flex items-center gap-2 hover:underline text-muted-foreground"
           >
             <GitHubLogoIcon className="h-5 w-5" />
             GitHub
           </Link>
         </div>
         <div className="grid gap-1">
-          <h3 className="font-semibold">{t("Legal")}</h3>
-          <Link href="/privacy-policy" className="hover:underline">
+          <h3 className="font-bold">{t("Legal")}</h3>
+          <Link
+            href="/privacy-policy"
+            className="hover:underline text-muted-foreground"
+          >
             {t("PrivacyPolicy")}
           </Link>
-          <Link href="/term-of-service" className="hover:underline">
+          <Link
+            href="/term-of-service"
+            className="hover:underline text-muted-foreground"
+          >
             {t("TermOfService")}
           </Link>
         </div>
         <div className="grid col-span-2">
-          <p className="text-gray-500 dark:text-gray-400">
-            © 2024 hockeypool.live
-          </p>
+          <p className="text-muted-foreground">© 2024 hockeypool.live</p>
         </div>
       </div>
     </footer>
