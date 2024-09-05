70d4c4111b19120ce3505113f8421d84c733bbd0
Jean-François Corriveau <jcorriveau23@gmail.com>
2024-09-05 18:11:16 -0400

    remove un done tab

--- src/app/[locale]/pool/[name]/dynasty/dynasty-pool.tsx
+++ src/app/[locale]/pool/[name]/dynasty/dynasty-pool.tsx
@@ -100,9 +100,9 @@ export default function DynastyPool() {
         <div className="overflow-auto">
           <TabsList>
             <TabsTrigger value={DynastyTabs.ROSTER}>{t("Roster")}</TabsTrigger>
-            <TabsTrigger value={DynastyTabs.NHL_PLAYERS}>
+            {/* <TabsTrigger value={DynastyTabs.NHL_PLAYERS}>
               {t("NhlPlayers")}
-            </TabsTrigger>
+            </TabsTrigger> */}
             <TabsTrigger value={DynastyTabs.DRAFT}>{t("Draft")}</TabsTrigger>
             <TabsTrigger value={DynastyTabs.SETTINGS}>
               {t("Settings")}
@@ -112,9 +112,9 @@ export default function DynastyPool() {
         <TabsContent value={DynastyTabs.ROSTER}>
           <RosterTab />
         </TabsContent>
-        <TabsContent value={DynastyTabs.NHL_PLAYERS}>
+        {/* <TabsContent value={DynastyTabs.NHL_PLAYERS}>
           <NhlPlayersTab />
-        </TabsContent>
+        </TabsContent> */}
         <TabsContent value={DynastyTabs.DRAFT}>
           <DraftTab />
         </TabsContent>
