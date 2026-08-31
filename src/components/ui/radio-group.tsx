2f966d9bd3004f5497557a71416afe1e9a288b22
Jean-François Corriveau <jcorriveau23@gmail.com>
2026-08-31 11:39:25 -0400

    added functionality to update pooler name

--- src/components/ui/radio-group.tsx
+++ src/components/ui/radio-group.tsx
@@ -3,7 +3,6 @@
 import * as React from "react";
 import { RadioGroup as RadioGroupPrimitive } from "@base-ui/react/radio-group";
 import { Radio as RadioPrimitive } from "@base-ui/react/radio";
-import { Circle } from "lucide-react";
 
 import { cn } from "@/lib/utils";
 
@@ -29,13 +28,13 @@ const RadioGroupItem = React.forwardRef<
     <RadioPrimitive.Root
       ref={ref}
       className={cn(
-        "aspect-square h-4 w-4 rounded-full border border-primary text-primary ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 data-disabled:cursor-not-allowed data-disabled:opacity-50",
-        className
+        "relative aspect-square size-4 shrink-0 rounded-full border border-input ring-offset-background focus:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 data-checked:border-primary data-disabled:cursor-not-allowed data-disabled:opacity-50",
+        className,
       )}
       {...props}
     >
-      <RadioPrimitive.Indicator className="flex items-center justify-center">
-        <Circle className="h-2.5 w-2.5 fill-current text-current" />
+      <RadioPrimitive.Indicator>
+        <span className="absolute left-1/2 top-1/2 size-2 -translate-x-1/2 -translate-y-1/2 rounded-full bg-primary" />
       </RadioPrimitive.Indicator>
     </RadioPrimitive.Root>
   );
